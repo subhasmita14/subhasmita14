5d3d75a1edf33ce3be1d9a4a30e1c22d4f1eff15
Subhasmita <subhasmt02@gmail.com>
2021-12-19 18:31:11 +0300

    rectangle program

---
+++ rectangle.py
@@ -0,0 +1,12 @@
+#Program to draw square in Python Turtle
+import turtle
+ 
+t = turtle.Turtle()
+t.forward(100) #Forward turtle by 100 units
+t.left(90) #Turn turtle by 90 degree
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
